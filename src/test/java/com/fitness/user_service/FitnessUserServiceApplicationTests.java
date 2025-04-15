ac93e93fcf40b6b2da9d16585e051de9a95864cd
Nageswarareddy Chaduvula <dt241115@ssnc-corp.global>
2025-04-15 11:30:23 +0530

    initial commit

---
+++ src/test/java/com/fitness/user_service/FitnessUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitness.user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
